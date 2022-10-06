356443c5e384f78a2484c725ea2a0a7332fe70a4
ErosEros <eros.vittori1994@gmail.com>
2022-10-06 21:51:25 +0200

    add menu in headerComponent with props

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,53 @@
 <template>
-  <div id="app">
-   
-  </div>
+  <HeaderComponent :links="menu"/>
+
 </template>
 
+
 <script>
+import HeaderComponent from "./components/HeaderComponent.vue";
 
 
 export default {
-  name: 'App',
-
+    components: { 
+      HeaderComponent 
+    },
+    data(){
+      return {
+        menu: [
+          {
+              text: "HOME",
+              url: "#",
+              current: false,
+          },
+          {
+              text: "ABOUT",
+              url: "#",
+              current: false,
+          },
+          {
+              text: "SERVICE",
+              url: "#",
+              current: false,
+          },
+          {
+              text: "SHOWCASE",
+              url: "#",
+              current: false,
+          },
+          {
+              text: "BLOG",
+              url: "#",
+              current: false,
+          },
+          {
+              text: "CONTACT",
+              url: "#",
+              current: false,
+          },
+        ]
+      }
+    }
 }
 </script>
 
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="container-header">
+        <div class="header-top">
+            HeaderComponent 
+        </div>
+        <div class="header-bottom">
+            <div class="row">
+                <div class="col-3 text-center">
+                    <span><strong>Flox | Business</strong></span>
+                </div>
+                <div class="col-6">
+                    <ul class="d-flex justify-content-around">
+                        <li v-for="(link, index) in links" :key="index">
+                        <a :class="{active: link.current }" :href="link.url">
+                        {{ link.text }}
+                        </a>
+                    </li>
+
+                    
+
+                        <!-- <li>HOME</li>
+                        <li>ABOUT</li>
+                        <li>SERVICE</li>
+                        <li>SHOWCASE</li>
+                        <li>BLOG</li>
+                        <li>CONTACT</li> -->
+                    </ul>
+                </div>
+                <div class="col-3 text-center">
+                    <button class="btn btn-primary" type="button">Button</button>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+
+</template>
+<script>
+export default {
+    name: "HeaderComponent",
+    props: ["links"],
+}
+</script>
+<style lang="scss">
+    .header-top{
+        background-color: #4c4c4c;
+        height: 5vh;
+    }
+    .header-bottom {
+        height: 10vh;
+        border: 1px solid black
+    }
+
+    .li {
+        list-style: none
+    }
+</style>
