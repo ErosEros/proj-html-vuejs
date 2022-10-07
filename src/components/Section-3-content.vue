8e93ce9957007417707883754457e7cb13d30198
ErosEros <eros.vittori1994@gmail.com>
2022-10-07 14:57:54 +0200

    add section-3

--- src/components/Section-3-content.vue
+++ src/components/Section-3-content.vue
@@ -2,7 +2,7 @@
     <div class="container-section-3">
         <div class="row">
             <div class="col-8">
-                <div class="row">
+                <div class="row mt-5 mb-5">
                     <div id="bg-eros-green" class="col-6">
                         <img class="opacity-eros" src="./img/busy-architect-PYVKWM4-1024x872.jpg" alt="architect">
                     </div>
@@ -13,7 +13,9 @@
                 </div>           
             </div>
             <div class="col-4">
-                
+                <h5>Lear more about our missions</h5>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora odio omnis natus iusto. Ea velit nam asperiores molestiae nesciunt dicta, dolorem possimus enim tempora nemo. Tempore vitae itaque architecto cumque.</p>
+                <button type="button" class="btn btn-success mt-4 mb-4 ">Success</button>
             </div>
         </div>
     </div>
@@ -35,5 +37,10 @@ export default {
     #bg-eros-green{
     background: linear-gradient(45deg, #00d916, transparent);
     border-top-right-radius: 5%
+    }
+
+    .container-section-3{
+        margin: 20px 0px;
+        background-color: #f5f5f5;
     }
 </style>
